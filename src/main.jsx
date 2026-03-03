9ea848e09e5588acaa2ce681fa9cd72a0cd326cb
Aight644 <hamza.abubakar893@gmail.com>
2026-03-03 16:55:29 +1000

    stage-based auto-combat, growth stats, equipment summoning & merge system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './storage.js'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BladeRealmsApp from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BladeRealmsApp />
   </React.StrictMode>
-)
+);
